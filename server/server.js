fd3713c67e9a47f191e2554051b131b303161bba
Mokhtar Naamani <mokhtar.naamani@gmail.com>
2015-06-18 09:56:04 -0700

    fix static client folder path

--- server/server.js
+++ server/server.js
@@ -1,18 +1,18 @@
 var express = require('express');
 var partials = require('express-partials');
 var bodyParser = require('body-parser');
-
+var path = require('path');
 
 var app = express();
 
-app.set('views', __dirname + '/views');
+app.set('views', path.join(__dirname, "views"));
 app.set('view engine', 'ejs');
 app.use(partials());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 //static content
-app.use(express.static(__dirname + '/client'));
+app.use(express.static(path.join(__dirname, "../client")));
 
 //angular SPA
 app.get('/', function(req, res) {
