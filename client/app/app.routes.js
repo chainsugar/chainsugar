ff0180b07da30c239143bbb46a2327d7d4f7adea
bryan <bry_angelo@yahoo.com>
2015-06-18 15:58:11 -0700

    added new routes

--- client/app/app.routes.js
+++ client/app/app.routes.js
@@ -4,10 +4,16 @@
     
     .config(function($routeProvider) {
     
-      $routeProvider.when('/', {
+    $routeProvider.when('/', {
           templateUrl: 'app/components/landing/landing.html',
           controller: 'LandingPageController'
       })
+
+    $routeProvider.when('/sign-in', {
+          templateUrl: 'app/components/sign-in/sign-in.html',
+          controller: 'SignInController'
+      })
+
       .otherwise({redirectTo: '/'});
       
     });
