d1f3d064f4f05f4d55de466304d5d4207a25b063
chiangchendah <chiangchendah@gmail.com>
2015-06-20 02:17:23 +0800

    recursively search in shared directory

--- Gruntfile.js
+++ Gruntfile.js
@@ -14,7 +14,7 @@ module.exports = function(grunt) {
         separator: ';',
       },
       dist: {
-        src: ['client/app/components/**/*.js', 'client/app/shared/*.js'],
+        src: ['client/app/components/**/*.js', 'client/app/shared/**/*.js'],
         dest: 'client/dist/app.js',
       },
     }
