786a89a06b9f4384e745ffd643d8e4f4be648439
Mokhtar Naamani <mokhtar.naamani@gmail.com>
2015-06-18 23:25:26 -0700

    created User and Task mongoose models, user saved to db when they login

---
+++ server/db/index.js
@@ -0,0 +1,9 @@
+var mongoose = require('mongoose');
+var config = require('../config.js');
+
+mongoose.connect(config.MONGODB_URL);
+
+module.exports = {
+  User: require('./user.js'),
+  Task: require('./task.js')
+}
---
+++ server/db/user.js
@@ -0,0 +1,8 @@
+var mongoose = require('mongoose');
+
+var UserSchema = new mongoose.Schema({
+  googleId: {type: String, index: { unique: true } },
+  name: String
+});
+
+module.exports = mongoose.model('User', UserSchema);
