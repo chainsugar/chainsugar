0b437313e22b232da825e9f7619b6c0bfa1f3cec
chiangchendah <chiangchendah@gmail.com>
2015-06-20 03:24:18 +0800

    add watch task to rebuild and rerun server

--- Gruntfile.js
+++ Gruntfile.js
@@ -17,18 +17,29 @@ module.exports = function(grunt) {
         src: ['client/app/components/**/*.js', 'client/app/shared/**/*.js'],
         dest: 'client/dist/app.js',
       },
+    },
+
+    watch: {
+      scripts: {
+        files: ['client/app/components/**/*.js', 'client/app/shared/**/*.js'],
+        tasks: ['build', 'nodemon:dev'],
+        options: {
+          spawn: false,
+        },
+      }
     }
 
   });
 
   grunt.loadNpmTasks('grunt-nodemon');
   grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-watch');
 
   //gets run on server deployment
   grunt.registerTask('build', [
     'concat'
   ]);
 
-  grunt.registerTask('default', ['nodemon:dev']);
+  grunt.registerTask('default', ['watch', 'nodemon:dev']);
 
 };
