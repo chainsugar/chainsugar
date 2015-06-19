c1b5514f550fae6322309119da15e6c47be2d000
Mokhtar Naamani <mokhtar.naamani@gmail.com>
2015-06-19 12:39:20 -0700

    updated helper method on scope for the form

--- client/app/components/tasks/task-formController.js
+++ client/app/components/tasks/task-formController.js
@@ -3,11 +3,15 @@
   //load module
   angular.module('trApp')
     .controller('TaskFormController', ['$scope', '$http', '$location', 'TaskFormService', TaskFormController]);
-      
+
   function TaskFormController($scope, $http, $location, TaskFormService){
     $scope.form = {};
+
     // http POST on form submit
-    $scope.createTask = TaskFormService.addTask($scope.form);
+    $scope.createTask = function(){
+      TaskFormService.addTask($scope.form);
+    };
+
     $scope.redirectCall = function() {
       $location.path('/task-view');
     };
