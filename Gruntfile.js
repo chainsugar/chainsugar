531d38929b932b367d827cef448dbcf992e7d2b6
chiangchendah <chiangchendah@gmail.com>
2015-06-20 00:59:59 +0800

    add concat task

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,15 +7,26 @@ module.exports = function(grunt) {
       dev: {
         script: 'server/server.js'
       }
+    },
+
+    concat: {
+      options: {
+        separator: ';',
+      },
+      dist: {
+        src: ['client/app/components/**/*.js'],
+        dest: 'client/assets/main-concat.js',
+      },
     }
 
   });
 
   grunt.loadNpmTasks('grunt-nodemon');
+  grunt.loadNpmTasks('grunt-contrib-concat');
 
   //gets run on server deployment
   grunt.registerTask('build', [
-
+    'concat'
   ]);
 
   grunt.registerTask('default', ['nodemon:dev']);
