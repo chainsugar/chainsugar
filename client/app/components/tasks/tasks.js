6c18079979c206096c692d4b1e67d2bc1eef547e
bryan <bry_angelo@yahoo.com>
2015-06-22 16:11:05 -0700

    Updated table headings, added retrieverAllTasks

--- client/app/components/tasks/tasks.js
+++ client/app/components/tasks/tasks.js
@@ -1,10 +1,22 @@
 (function(){
+
 angular.module('trApp')
     .controller('TasksController', ['$scope', 'TaskFormService', TasksController]);
 
   function TasksController($scope, TaskFormService){
    // make calls to TaskFormService to retrieve all tasks
-   $scope.tasks = [ { owner: 'Bryan', information: {cost: '10', name: 'Walk The Dog', city: 'San Francisco', description: 'Easy Money'}} ];
+   
   };
 
 })();
+
+// $scope.tasks = [{
+   //   owner: 'Bryan', 
+   //   information: {
+   //     cost: '10', 
+   //     name: 'Walk The Dog', 
+   //     city: 'San Francisco', 
+   //     description: 'Easy Money'
+   //   },
+   //   applicants:[] 
+   // }];
