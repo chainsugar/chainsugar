0874fccf5b1bb933ccbe18cde32e8f0e9812b7c3
bryan <bry_angelo@yahoo.com>
2015-06-22 15:48:52 -0700

    Home views page updated, fixed controller files

--- client/app/components/tasks/tasks.js
+++ client/app/components/tasks/tasks.js
@@ -1,8 +1,10 @@
- angular.module('trApp')
+(function(){
+angular.module('trApp')
     .controller('TasksController', ['$scope', 'TaskFormService', TasksController]);
 
   function TasksController($scope, TaskFormService){
    // make calls to TaskFormService to retrieve all tasks
+   $scope.tasks = [ { owner: 'Bryan', information: {cost: '10', name: 'Walk The Dog', city: 'San Francisco', description: 'Easy Money'}} ];
   };
 
 })();
