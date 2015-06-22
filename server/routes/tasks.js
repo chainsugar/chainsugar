eaa0a3570303ad24e9eec8dcbf5f5e76b43c7759
Mokhtar Naamani <mokhtar.naamani@gmail.com>
2015-06-22 16:50:11 -0700

    added /api/mytasks

--- server/routes/tasks.js
+++ server/routes/tasks.js
@@ -1,16 +1,47 @@
 var db = require("../db/");
+var _ = require("underscore");
 
 module.exports = function(app, express) {
 
   //return list of all tasks
   app.get('/api/tasks', isAuthenticated, function(req, res){
-    db.Task.find().exec(function(err, tasks){
+    db.Task.find().lean().exec(function(err, tasks){
       if(err) {
         res.status(500);
       } else {
+        tasks = _.map(tasks, function(task){
+          task.isOwner = task.owner === req.user._id;
+          return task;
+        });
         res.status(200).send(tasks);
       }
     });
+  });
+
+  //return list of all user taks
+  //wher user is owner, assigned to a task or is an applicant
+  //adds additional boolean properties on each task
+  //'isOwner', 'isAssignedToMe', 'appliedTo'
+  app.get('/api/mytasks', isAuthenticated, function(req, res){
+    db.Task.find({$or:[
+        {owner: req.user._id},
+        {assignedTo: req.user._id},
+        {applicants: req.user._id}
+      ]})
+      .lean()
+      .exec(function(err, tasks){
+        if(err) {
+          res.status(500);
+        } else {
+          tasks = _.map(tasks, function(task){
+            task.isOwner = task.owner === req.user._id;
+            task.isAssignedToMe = task.assignedTo === req.user._id;
+            task.appliedTo = _.contains(tasks.applicants, req.user._id);
+            return task;
+          });
+          res.status(200).send(tasks);
+        }
+      });
   });
 
   //create new task
