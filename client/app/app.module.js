58578a7a7ac2bf714c5f6186726bf073208dbe88
Mokhtar Naamani <mokhtar.naamani@gmail.com>
2015-06-23 22:54:57 -0700

    fixed redirect to sign-in page when arriving on landing page

--- client/app/app.module.js
+++ client/app/app.module.js
@@ -4,6 +4,11 @@ angular.module('trApp', ['ngRoute'])
   .run(function ($rootScope, $location, AuthService) {
     $rootScope.$on('$routeChangeStart', function (evt, next, current) {
 
+      //ignore arriving at root path
+      if (next.$$route.originalPath === "") {
+        return;
+      }
+
       if (next.$$route && !next.$$route.publicAccess){
         AuthService.check().then(function(loggedIn){
           if(!loggedIn) $location.path('/sign-in');
