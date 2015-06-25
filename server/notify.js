7dc1aca3ddcb3d69f5bea0fcb7b532939a1f015f
Mokhtar Naamani <mokhtar.naamani@gmail.com>
2015-06-25 03:02:08 -0700

    added notify service

---
+++ server/notify.js
@@ -0,0 +1,10 @@
+var db = require("./db");
+var emailService = require("./email");
+
+module.exports.newApplication = function(taskId) {
+
+};
+
+module.exports.taskAssigned = function(taskId) {
+
+};
