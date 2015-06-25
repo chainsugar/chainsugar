259ebef5f885be613ef61c393663a3ee30719c24
Mokhtar Naamani <mokhtar.naamani@gmail.com>
2015-06-25 02:40:34 -0700

    adding sendgrid email service

--- server/config.js
+++ server/config.js
@@ -2,31 +2,36 @@
 
 if (process.env.NODE_ENV === 'production') {
   /** Production env **/
+  module.exports = {
+    //google app
+    "GOOGLE_APP_ID": process.env.GOOGLE_APP_ID,
+    "GOOGLE_APP_SECRET": process.env.GOOGLE_APP_SECRET,
+    "GOOGLE_APP_CALLBACK_URL": process.env.GOOGLE_APP_CALLBACK_URL,
 
-  //google app
-  module.exports.GOOGLE_APP_ID = process.env.GOOGLE_APP_ID;
-  module.exports.GOOGLE_APP_SECRET = process.env.GOOGLE_APP_SECRET;
-  module.exports.GOOGLE_APP_CALLBACK_URL = process.env.GOOGLE_APP_CALLBACK_URL;
+    //session key for signing session cookies
+    "SESSION_SECRET": process.env.SESSION_SECRET,
 
-  //session key for signing session cookies
-  module.exports.SESSION_SECRET = process.env.SESSION_SECRET;
+    //mongo connection url
+    "MONGODB_URL": process.env.MONGODB_URL,
 
-  //mongo connection url
-  module.exports.MONGODB_URL = process.env.MONGODB_URL;
+    //azure sengrid cloud email delivery service
+    "SENDGRID_USERNAME": process.env.SENDGRID_USERNAME,
+    "SENDGRID_PASSWORD": process.env.SENDGRID_PASSWORD
+  };
 
 } else {
 
   /** Development env **/
   try {
     var googleApp = require('../secrets/google.json');
+    module.exports = {
+      "GOOGLE_APP_ID": googleApp.id,
+      "GOOGLE_APP_SECRET": googleApp.secret,
+      "GOOGLE_APP_CALLBACK_URL": googleApp.url,
 
-    module.exports.GOOGLE_APP_ID = googleApp.id;
-    module.exports.GOOGLE_APP_SECRET = googleApp.secret;
-    module.exports.GOOGLE_APP_CALLBACK_URL = googleApp.url;
-
-    module.exports.SESSION_SECRET = "w34587230598dflskjf";
-    module.exports.MONGODB_URL = "mongodb://localhost/taskbunny";
-
+      "SESSION_SECRET": "w34587230598dflskjf",
+      "MONGODB_URL": "mongodb://localhost/taskbunny"
+    };
   } catch(e) {
     console.error("== Warning == missing 'secrets/google.json' > see README");
     process.exit();
---
+++ server/email.js
@@ -0,0 +1,39 @@
+var config = require("./config");
+
+//https://azure.microsoft.com/en-us/documentation/articles/store-sendgrid-nodejs-how-to-send-email/
+
+var sendGridUsername = config.SENDGRID_USERNAME;
+var sendGridPassword = config.SENDGRID_PASSWORD;
+
+if (sendGridUsername && sendGridPassword) {
+
+  var sendgrid = require('sendgrid')(sendGridUsername, sendGridPassword);
+
+  module.exports.send = function(opt, callback) {
+
+    var email = new sendgrid.Email(opt);
+    sendgrid.send(email, callback);
+
+  };
+
+  module.exports.sendTestEmail = function(to, callback) {
+
+    var email = new sendgrid.Email({
+      to: to,
+      from: "noreply@taskbunny.com",
+      subject: "test message from taskbunny",
+      text: "This is a test from TaskBunny"
+    });
+
+    sendgrid.send(email, callback);
+  };
+
+} else {
+
+  module.exports.send = function(opt, callback) {
+    process.nextTick(function(){
+      callback();
+    });
+  };
+
+}
